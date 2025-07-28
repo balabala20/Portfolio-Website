3b1281ea69a1e1d0e198519498356a9a24b88512
Balasiva <DOPATHIBALASIVA@gmail.com>
2025-07-28 21:31:31 +0530

    Update adminPage.js

--- frontend/src/adminPage.js
+++ frontend/src/adminPage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-const base_url = `https://portfolio-website-backend-ashy.vercel.app/`;
+const base_url = `https://portfolio-website-backend-ashy.vercel.app`;
 
 const AdminPage = () => {
   const [data, setData] = useState([]);
