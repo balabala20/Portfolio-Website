78d9793ac33fbf57d47599565f56833317788b71
balasiva <dopathibalasiva@gmail.com.com>
2025-09-04 22:12:51 +0530

    create dotenv

--- frontend/src/adminPage.js
+++ frontend/src/adminPage.js
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-
-const base_url = `https://portfolio-website-backend-qaqf.onrender.com`;
+const base_url = process.env.React_App_Backend_Url;
 
 const AdminPage = () => {
   const [data, setData] = useState([]);
